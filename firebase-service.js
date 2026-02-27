82c606dd66b08bf5c73d61f7a45f4c8eb834b66c
Elmo <elmomota2019@gmail.com>
2026-02-26 23:32:06 -0300

    fix: prevent crash when FIREBASE_PRIVATE_KEY env var is undefined

--- firebase-service.js
+++ firebase-service.js
@@ -15,7 +15,7 @@ const serviceAccountKey = {
   type: process.env.FIREBASE_TYPE || process.env.TYPE || 'service_account',
   project_id: process.env.FIREBASE_PROJECT_ID || process.env.PROJECT_ID,
   private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID || process.env.PRIVATE_KEY_ID,
-  private_key: (process.env.FIREBASE_PRIVATE_KEY || process.env.PRIVATE_KEY).replace(/\\n/g, "\n"),
+  private_key: (process.env.FIREBASE_PRIVATE_KEY || process.env.PRIVATE_KEY || '').replace(/\\n/g, "\n"),
   client_email: process.env.FIREBASE_CLIENT_EMAIL || process.env.CLIENT_EMAIL,
   client_id: process.env.FIREBASE_CLIENT_ID || process.env.CLIENT_ID,
   auth_uri: process.env.FIREBASE_AUTH_URI || process.env.AUTH_URI,
